4589d5364b5f672e4c2fd3a7d09a7de301cf9f7b
atks <adriantks@gmail.com>
2023-06-30 16:47:49 +0800

    misc changes

--- cleanup/extract_sanger_sequence_from_mbox.py
+++ cleanup/extract_sanger_sequence_from_mbox.py
@@ -6,7 +6,7 @@
 # of this software and associated documentation files (the 'Software'), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permsit persons to whom the Software is
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
@@ -33,8 +33,16 @@ from shutil import rmtree
 
 
 @click.command()
-@click.option('-s', '--src_dir', default=os.getcwd(), show_default=True, help='working directory')
-@click.option('-d', '--dst_dir', default='/data/var/backup/categories/sanger', show_default=True, help='working directory')
+@click.option(
+    "-s", "--src_dir", default=os.getcwd(), show_default=True, help="working directory"
+)
+@click.option(
+    "-d",
+    "--dst_dir",
+    default="/data/var/backup/categories/sanger",
+    show_default=True,
+    help="working directory",
+)
 def main(src_dir, dst_dir):
     """
     Looks for all .ab1 and .seq files in source directory and copies it to destination directory
@@ -57,26 +65,26 @@ def main(src_dir, dst_dir):
         for file in filenames:
             # print(file)
             # print(f'\t{dirpath}/{file}')
-            if file.endswith('.ab1'):
+            if file.endswith(".ab1"):
                 no_ab1 += 1
                 ab1 = os.path.basename(file)
                 if ab1 in ab1_files.keys():
-                    if ab1_files[ab1].update(f'{dirpath}/{file}'):
+                    if ab1_files[ab1].update(f"{dirpath}/{file}"):
                         no_ab1_o += 1
                     else:
                         no_ab1_x += 1
                 else:
-                    ab1_files[ab1] = Record(f'{dirpath}/{file}')
-            elif file.endswith('.seq'):
+                    ab1_files[ab1] = Record(f"{dirpath}/{file}")
+            elif file.endswith(".seq"):
                 no_seq += 1
                 seq = os.path.basename(file)
                 if seq in seq_files.keys():
-                    if seq_files[seq].update(f'{dirpath}/{file}'):
+                    if seq_files[seq].update(f"{dirpath}/{file}"):
                         no_seq_o += 1
                     else:
                         no_seq_x += 1
                 else:
-                    seq_files[seq] = Record(f'{dirpath}/{file}')
+                    seq_files[seq] = Record(f"{dirpath}/{file}")
 
     ab1_collisions = [0 for i in range(25)]
     seq_collisions = [0 for i in range(25)]
@@ -87,34 +95,34 @@ def main(src_dir, dst_dir):
 
     for key in ab1_files.keys():
         src = ab1_files[key].file_path
-        dst = f'{dst_dir}/{ab1_files[key].base}'
+        dst = f"{dst_dir}/{ab1_files[key].base}"
         # copy2(src, dst)
         ab1_files[key].print_tab()
         for i, file in enumerate(ab1_files[key].collisions):
             src = file
-            dst = f'{dst_dir}/{ab1_files[key].base}.{i+1}'
+            dst = f"{dst_dir}/{ab1_files[key].base}.{i+1}"
             # copy2(src, dst)
 
     for key in seq_files.keys():
         src = seq_files[key].file_path
-        dst = f'{dst_dir}/{seq_files[key].base}'
+        dst = f"{dst_dir}/{seq_files[key].base}"
         # copy2(src, dst)
         seq_files[key].print_tab()
         for i, file in enumerate(seq_files[key].collisions):
             src = file
-            dst = f'{dst_dir}/{seq_files[key].base}.{i+1}'
+            dst = f"{dst_dir}/{seq_files[key].base}.{i+1}"
             # copy2(src, dst)
 
-    print(f'ab1 files observed  : {no_ab1}')
-    print(f'unique ab1 files    : {len(ab1_files.keys())}')
-    print(f'seq files observed  : {no_seq}')
-    print(f'unique seq files    : {len(seq_files.keys())}')
-    print(f'no_ab1_o            : {no_ab1_o}')
-    print(f'no_ab1_x            : {no_ab1_x}')
-    print(f'no_seq_o            : {no_seq_o}')
-    print(f'no_seq_x            : {no_seq_x}')
-    print(f'ab1 collisions      : {ab1_collisions}')
-    print(f'seq collisions      : {seq_collisions}')
+    print(f"ab1 files observed  : {no_ab1}")
+    print(f"unique ab1 files    : {len(ab1_files.keys())}")
+    print(f"seq files observed  : {no_seq}")
+    print(f"unique seq files    : {len(seq_files.keys())}")
+    print(f"no_ab1_o            : {no_ab1_o}")
+    print(f"no_ab1_x            : {no_ab1_x}")
+    print(f"no_seq_o            : {no_seq_o}")
+    print(f"no_seq_x            : {no_seq_x}")
+    print(f"ab1 collisions      : {ab1_collisions}")
+    print(f"seq collisions      : {seq_collisions}")
 
 
 class Record(object):
@@ -138,11 +146,11 @@ class Record(object):
         print(f'{self.base}\t{self.n}\t{self.file_path}{":".join(self.collisions)}')
 
     def print(self):
-        print(f'++++++++++++++++++++')
-        print(f'base       : {self.base}')
-        print(f'n          : {self.n}')
-        print(f'path       : {self.file_path}')
-        print(f'++++++++++++++++++++')
+        print(f"++++++++++++++++++++")
+        print(f"base       : {self.base}")
+        print(f"n          : {self.n}")
+        print(f"path       : {self.file_path}")
+        print(f"++++++++++++++++++++")
 
 
 main()
--- cleanup/remove_trinity.py
+++ cleanup/remove_trinity.py
@@ -6,7 +6,7 @@
 # of this software and associated documentation files (the 'Software'), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permsit persons to whom the Software is
+# copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
 # The above copyright notice and this permission notice shall be included in
 # all copies or substantial portions of the Software.
@@ -30,46 +30,66 @@ from pathlib import Path
 from shutil import copy2
 from shutil import rmtree
 
-def main():
 
+def main():
     # options
     parser = CAVSParser(
-        description='Removes trinity folders from backups.',
+        description="Removes trinity folders from backups.",
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''\
+        epilog=textwrap.dedent(
+            """\
            usage: remove_trinity <dir>
 
-           '''))
-    parser.add_argument('dir', help='directory to scan for trinity subfolder folders')
-    parser.add_argument('-l', '--level', default=0, type=int, required=False,  help='Level of action, 0 for dry run, 1 for copy, 2 for copy and delete')
+           """
+        ),
+    )
+    parser.add_argument("dir", help="directory to scan for trinity subfolder folders")
+    parser.add_argument(
+        "-l",
+        "--level",
+        default=0,
+        type=int,
+        required=False,
+        help="Level of action, 0 for dry run, 1 for copy, 2 for copy and delete",
+    )
     args = parser.parse_args()
 
     for arg in vars(args):
-        print('\t{0:<20} :   {1:<10}'.format(arg, getattr(args, arg)))
+        print("\t{0:<20} :   {1:<10}".format(arg, getattr(args, arg)))
 
     level = args.level
 
     no_read_partitions = 0
-    if not exists('read_partitions_path.txt'):
-        #scan all directories
+    if not exists("read_partitions_path.txt"):
+        # scan all directories
         print("Searching for read_partitions")
-        print(f"find {args.dir} -type d -name \"read_partitions\"")
-        dirs = subprocess.run(f"find {args.dir} -type d -name \"read_partitions\"", text=True, shell=True, capture_output=True)
+        print(f'find {args.dir} -type d -name "read_partitions"')
+        dirs = subprocess.run(
+            f'find {args.dir} -type d -name "read_partitions"',
+            text=True,
+            shell=True,
+            capture_output=True,
+        )
         print(dirs.stdout)
         no_read_partitions = 0
-        f = open('read_partitions_path.txt', 'w')
-        dir_list = dirs.stdout.split('\n')
+        f = open("read_partitions_path.txt", "w")
+        dir_list = dirs.stdout.split("\n")
         for i, path in enumerate(dir_list):
             if i < len(dir_list) - 1:
-                f.write(f'{path}\n')
+                f.write(f"{path}\n")
             else:
                 f.write(path)
             no_read_partitions += 1
         f.close()
     else:
-        no_read_partitions = subprocess.run(f"wc -l read_partitions_path.txt", text=True, shell=True, capture_output=True).stdout
+        no_read_partitions = subprocess.run(
+            f"wc -l read_partitions_path.txt",
+            text=True,
+            shell=True,
+            capture_output=True,
+        ).stdout
 
-    print(f'Found {no_read_partitions} read partitions')
+    print(f"Found {no_read_partitions} read partitions")
 
     no_dirs = 0
     no_dirs_with_fasta = 0
@@ -81,29 +101,29 @@ def main():
     no_fasta_files_same = 0
     no_dirs_deleted = 0
 
-    file = open('read_partitions_path.txt')
+    file = open("read_partitions_path.txt")
     for line in file:
-        if line.endswith('read_partitions\n'):
+        if line.endswith("read_partitions\n"):
             dir_path = Path(line.rstrip()[:-15:])
             print(dir_path)
         else:
-            print(f'UNEXPECTED DIR ENCOUNTERED : {line}')
+            print(f"UNEXPECTED DIR ENCOUNTERED : {line}")
             break
 
-        #look for contig file
+        # look for contig file
         fasta_file = None
         for file in os.listdir(dir_path):
-            if re.search('\.fasta$', file) is not None:
-                #print('\t' + file)
+            if re.search("\.fasta$", file) is not None:
+                # print('\t' + file)
                 fasta_file = file
 
-        #move fasta file up one directory
-        file_has_been_copied =  False
-        no_fasta_file =  False
+        # move fasta file up one directory
+        file_has_been_copied = False
+        no_fasta_file = False
         if fasta_file is not None:
             no_dirs_with_fasta += 1
             parent_dir_path = dir_path.parent.absolute()
-            fasta_size = os.stat(f'{dir_path}/{fasta_file}').st_size
+            fasta_size = os.stat(f"{dir_path}/{fasta_file}").st_size
             total_fasta_size += fasta_size
 
             src_file_path = dir_path.joinpath(fasta_file)
@@ -116,13 +136,13 @@ def main():
                     file_has_been_copied = True
             else:
                 if level == 1:
-                    print(f'copy {src_file_path} to {dst_dir_path}')
+                    print(f"copy {src_file_path} to {dst_dir_path}")
                     copy2(src_file_path, dst_dir_path)
                     file_has_been_copied = True
 
                 total_fasta_files_copied += 1
         else:
-            print(f'NO FASTA: {dir_path}')
+            print(f"NO FASTA: {dir_path}")
             no_fasta_file = True
             no_dirs_without_fasta += 1
 
@@ -131,7 +151,7 @@ def main():
         try:
             if level == 2:
                 if file_has_been_copied or no_fasta_file:
-                    print(f'delete {dir_path}')
+                    print(f"delete {dir_path}")
                     rmtree(dir_path)
                     no_dirs_deleted += 1
         except OSError as ex:
@@ -140,23 +160,24 @@ def main():
     total_fasta_size /= 1000000000
     total_dir_size /= 1000000000
 
-    print(f'===========================')
-    print(f'statistics')
-    print(f'===========================')
-    print(f'#dirs:              {no_dirs}')
-    print(f'   #with fasta:     {no_dirs_with_fasta}')
-    print(f'   #w/o  fasta:     {no_dirs_without_fasta}')
-    print(f'total fasta size:   {total_fasta_size}GB')
-    print(f'total dir size:     {total_dir_size}GB')
-    print(f'total fasta copied: {total_fasta_files_copied}')
-    print(f'total fasta exist:  {total_fasta_files_exist}')
-    print(f'   #no_same:        {no_fasta_files_same}')
-    print(f'no dir deleted:     {no_dirs_deleted}')
+    print(f"===========================")
+    print(f"statistics")
+    print(f"===========================")
+    print(f"#dirs:              {no_dirs}")
+    print(f"   #with fasta:     {no_dirs_with_fasta}")
+    print(f"   #w/o  fasta:     {no_dirs_without_fasta}")
+    print(f"total fasta size:   {total_fasta_size}GB")
+    print(f"total dir size:     {total_dir_size}GB")
+    print(f"total fasta copied: {total_fasta_files_copied}")
+    print(f"total fasta exist:  {total_fasta_files_exist}")
+    print(f"   #no_same:        {no_fasta_files_same}")
+    print(f"no dir deleted:     {no_dirs_deleted}")
 
     # for line in dirs.stdout.splitlines():
     #     print(line)
 
-def get_dir_size(path='.'):
+
+def get_dir_size(path="."):
     total = 0
     with os.scandir(path) as it:
         for entry in it:
@@ -166,10 +187,12 @@ def get_dir_size(path='.'):
                 total += get_dir_size(entry.path)
     return total
 
+
 class CAVSParser(argparse.ArgumentParser):
     def error(self, message):
-        sys.stderr.write('error: %s\n' % message)
+        sys.stderr.write("error: %s\n" % message)
         self.print_help()
         sys.exit(2)
+
 
 main()
