7a965fffe5b99a87a8eefd0b74da275821f93cae
atks <atks@umich.edu>
2024-07-18 14:59:55 +0800

    misc changes

--- var/20240716_spades_assembly_mode_comparisons/aggregate_nucmer_reports.py
+++ var/20240716_spades_assembly_mode_comparisons/aggregate_nucmer_reports.py
@@ -20,150 +20,64 @@
 
 import os
 import click
-from shutil import copy2
 
 
 @click.command()
-@click.option(
-    "-m",
-    "--make_file",
-    show_default=True,
-    default="run_assembly_comparisons_analysis.mk",
-    help="make file name",
-)
-@click.option(
-    "-w",
-    "--working_dir",
-    default=os.getcwd(),
-    show_default=True,
-    help="working directory",
-)
-@click.option("-s", "--sample_file", required=True, help="sample file")
-def main(make_file, working_dir, sample_file):
+@click.argument("report_files", nargs=-1)
+@click.option("-t", "--tag", required=True, help="tag")
+@click.option("-o", "--output_file", required=True, help="output data text file")
+def main(report_files, tag, output_file):
     """
-    Generates assembly comparisons statistics.
+    aggregate_nucmer_reports.py
 
-    e.g. generate_assembly_comparisons_pipeline.py
+    e.g. aggregate_nucmer_reports.py 1.report 2.report -t a_b -o a_b.txt
     """
-    print("\t{0:<20} :   {1:<10}".format("make_file", make_file))
-    print("\t{0:<20} :   {1:<10}".format("working_dir", working_dir))
-    print("\t{0:<20} :   {1:<10}".format("sample_file", sample_file))
-
-    # create directories in destination folder directory
-    pairwise_alignments_dir = f"{working_dir}/pairwise_alignments"
-    trace_dir = f"{working_dir}/trace"
-    try:
-        os.makedirs(pairwise_alignments_dir, exist_ok=True)
-        os.makedirs(trace_dir, exist_ok=True)
-    except OSError as error:
-        print(f"{error.filename} cannot be created")
-
-    nucmer = "/usr/local/mummer-4.0.0/bin/nucmer"
-    dnadiff = "/usr/local/mummer-4.0.0/bin/dnadiff"
-
-    # initialize
-    pg = PipelineGenerator(make_file)
+    print("\t{0:<20} :   {1:<10}".format("report_files", report_files))
+    print("\t{0:<20} :   {1:<10}".format("tag", tag))
+    print("\t{0:<20} :   {1:<10}".format("output_file", output_file))
 
     samples = []
     idx = 0
-    with open(sample_file, "r") as file:
-        for line in file:
-            if not line.startswith("#"):
-                idx += 1
-                sample_id, contigs_file = line.rstrip().split("\t")
-                samples.append(Sample(idx, sample_id, contigs_file))
-
-    samtools_multiqc_dep = ""
-
     #aggregate statistics from report
-    # NUCMER
-    #
-    #                             [REF]                [QRY]
-    # [Sequences]
-    # TotalSeqs                         51               264808
-    # AlignedSeqs             48(94.1176%)         490(0.1850%)
-    # UnalignedSeqs             3(5.8824%)     264318(99.8150%)
-    #
-    # [Bases]
-    # TotalBases                    148294             99993229
-    # AlignedBases        138783(93.5864%)      208972(0.2090%)
-    # UnalignedBases         9511(6.4136%)   99784257(99.7910%)
-    #
-    # [Alignments]
-    # 1-to-1                           138                  138
-    # TotalLength                   134376               134364
-    # AvgLength                   973.7391             973.6522
-    # AvgIdentity                  99.6538              99.6538
-    #
-    # M-to-M                           556                  556
-    # TotalLength                   227699               227687
-    # AvgLength                   409.5306             409.5090
-    # AvgIdentity                  95.9291              95.9291
+    # 1: NUCMER
+    # 2:
+    # 3:                             [REF]                [QRY]
+    # 4: [Sequences]
+    # 5: TotalSeqs                         51               264808
+    # 6: AlignedSeqs             48(94.1176%)         490(0.1850%)
+    # 7: UnalignedSeqs             3(5.8824%)     264318(99.8150%)
+    # 8:
+    # 9: [Bases]
+    #10: TotalBases                    148294             99993229
+    #11: AlignedBases        138783(93.5864%)      208972(0.2090%)
+    #12: UnalignedBases         9511(6.4136%)   99784257(99.7910%)
+    #13:
+    #14: [Alignments]
+    #15: 1-to-1                           138                  138
+    #16: TotalLength                   134376               134364
+    #17: AvgLength                   973.7391             973.6522
+    #18: AvgIdentity                  99.6538              99.6538
+    #19:
+    #20: M-to-M                           556                  556
+    #21: TotalLength                   227699               227687
+    #22: AvgLength                   409.5306             409.5090
+    #23: AvgIdentity                  95.9291              95.9291
+
+    #name, seq_type, total_seqs, aligned_seqs, unaligned_seqs, total_bases, aligned_bases, unaligned_bases, one_to_one, total_length, avg_length, avg_identity, m_to_m, total_length, avg_length, avg_identity
+
+    for f in report_files:
+        line_no = 0
+        with open(f, "r") as file:
+            for line in file:
+                if line_no == 1:
+                elif line_no == 1:
+
+                if not line.startswith("#"):
+                    idx += 1
+                    sample_id, contigs_file = line.rstrip().split("\t")
+                    samples.append(Sample(idx, sample_id, contigs_file))
 
 
-
-    pg.add_clean(f"rm -rf {pairwise_alignments_dir}")
-
-    # write make file
-    print("Writing pipeline")
-    pg.write()
-
-    # copy files to trace
-    copy2(__file__, trace_dir)
-    copy2(make_file, trace_dir)
-    copy2(sample_file, trace_dir)
-
-class PipelineGenerator(object):
-    def __init__(self, make_file):
-        self.make_file = make_file
-        self.tgts = []
-        self.deps = []
-        self.cmds = []
-        self.clean_cmd = ""
-
-    def add(self, tgt, dep, cmd):
-        self.tgts.append(tgt)
-        self.deps.append(dep)
-        self.cmds.append(cmd)
-
-    def add_clean(self, cmd):
-        self.clean_cmd = cmd
-
-    def print(self):
-        print(".DELETE_ON_ERROR:")
-        for i in range(len(self.tgts)):
-            print(f"{self.tgts[i]} : {self.deps[i]}")
-            print(f"\t{self.cmds[i]}")
-            print(f"\ttouch {self.tgts[i]}")
-
-    def write(self):
-        with open(self.make_file, "w") as f:
-            f.write(".DELETE_ON_ERROR:\n\n")
-            f.write("all : ")
-            for i in range(len(self.tgts)):
-                f.write(f"{self.tgts[i]} ")
-            f.write("\n\n")
-
-            for i in range(len(self.tgts)):
-                f.write(f"{self.tgts[i]} : {self.deps[i]}\n")
-                f.write(f"\t{self.cmds[i]}\n")
-                f.write(f"\ttouch {self.tgts[i]}\n\n")
-
-            if self.clean_cmd != "":
-                f.write(f"clean : \n")
-                f.write(f"\t{self.clean_cmd}\n")
-
-
-class Sample(object):
-    def __init__(self, idx, id, contigs_file):
-        self.idx = idx
-        self.id = id
-        self.contigs_file = contigs_file
-
-    def print(self):
-        print(f"idx           : {self.idx}")
-        print(f"id            : {self.id}")
-        print(f"contigs_file  : {self.contigs_file}")
 
 if __name__ == "__main__":
     main()  # type: ignore
