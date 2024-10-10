b93327f03e5eda707264419f463e2f6133c5deac
atks <atks@umich.edu>
2024-10-10 11:30:40 +0800

    updated prokka table filter

--- var/20240827_lsdv_index_case_sequence_submission/filter_prokka_tbl.py
+++ var/20240827_lsdv_index_case_sequence_submission/filter_prokka_tbl.py
@@ -60,28 +60,83 @@ def main(
     # get reference sequence
     ref_id = ""
     ref_seq = ""
-    with open(ref_fasta_file, "r") as f:
-        for line in f:
-            if line.startswith(">"):
-                ref_id = line.rstrip().split(" ")[0][1:]
-            else:
-                ref_seq += line.rstrip()
 
-    ref_len = len(ref_seq)
-    print("")
-    print(f"reference sequence")
-    print(f"header: {ref_id}")
-    print(f"length: {ref_len}")
+    with open(output_tbl_file, "w") as e:
+        with open(ref_fasta_file, "r") as f:
+            for line in f:
+                if line.startswith(">"):
+                    ref_id = line.rstrip().split(" ")[0][1:]
+                else:
+                    ref_seq += line.rstrip()
 
-    # parse input tbl file
-    feature_table = FeatureTable()
-    feature_table.parse(input_tbl_file)
+        ref_len = len(ref_seq)
+        print("")
+        print(f"reference sequence")
+        print(f"header: {ref_id}")
+        print(f"length: {ref_len}")
 
-    print("")
-    print(f"feature table parsed")
-    print(f"no records: {len(feature_table.features)}")
+        # parse input tbl file
+        feature_table = FeatureTable()
+        feature_table.parse(input_tbl_file)
+
+        print("")
+        print(f"feature table parsed")
+        print(f"no. records: {len(feature_table.features)}")
+
+        # filter features
+        no_genes = 0
+        no_stop_codons = 0
+        no_fwd_strand = 0
+        no_rev_strand = 0
+        for feature in feature_table.features:
+            #delete ab initio prediction annotation
+            is_gene = False
+
+            for annotation in feature.annotations:
+                if annotation.tag=="inference" and annotation.description.startswith("ab initio prediction"):
+                    feature.annotations.remove(annotation)
+                if annotation.tag=="gene":
+                    is_gene = True
+
+            if is_gene:
+                no_genes += 1
+
+                #check stop codon
+                codon = ""
+                is_stop_codon = False
+                if feature.end>feature.beg:
+                    no_fwd_strand += 1
+                    seq = ref_seq[feature.beg-1:feature.end]
+                    codon = ref_seq[feature.end-3:feature.end]
+                    if codon=="TAA" or codon=="TAG" or codon=="TGA":
+                        no_stop_codons += 1
+                        is_stop_codon = True
+                else:
+                    no_rev_strand += 1
+                    seq = ref_seq[feature.end-1:feature.beg]
+                    codon = ref_seq[feature.end-1:feature.end+2]
+                    if codon=="TTA" or codon=="CTA" or codon=="TCA":
+                        no_stop_codons += 1
+                        is_stop_codon = True
+
+                #check to ensure stop codon is present
+                if not is_stop_codon:
+                    seq = ref_seq[feature.end-1:feature.beg]
+                    print(f"seq: {seq}")
+                    print(f"codon: {codon}")
+
+                #edit gene annotation locus_tag
+                for annotation in feature.annotations:
+                    if annotation.tag=="locus_tag":
+                        annotation.description = f"EHIMFBMF_{no_genes}"
+
+                feature.write(e)
 
 
+    print(f"no. gene records: {no_genes}")
+    print(f"\tno. forward strand: {no_fwd_strand}")
+    print(f"\tno. reverse strand: {no_rev_strand}")
+    print(f"\tno. stop codons: {no_stop_codons}")
 class FeatureTable(object):
     def __init__(self):
         self.features = []
@@ -92,7 +147,6 @@ class FeatureTable(object):
             for line in f:
                 if line.startswith(">"):
                     continue
-
                 if re.search(r"^\d", line):
                     if len(feature_text)!=0:
                         feature = Feature()
@@ -117,20 +171,27 @@ class Feature(object):
         self.annotations = []
 
     def parse(self, text):
-        print(text)
+        #print(text)
         lines = text.split("\n")
-        self.beg, self.end, self.feature = lines[0].split("\t")
+        beg, end, self.feature = lines[0].split("\t")
+        self.beg = int(beg)
+        self.end = int(end)
         for line in lines[1:]:
             if len(line)==0:
                 continue
-            tag, description = line.split("\t")
+            tag, description = line.strip().split("\t")
             annotation = Annotation(tag, description)
             self.annotations.append(annotation)
 
     def print(self):
-        print(f"{self.beg}\t{self.beg}\t{self.feature}\n")
+        print(f"{self.beg}\t{self.end}\t{self.feature}")
         for annotation in self.annotations:
             annotation.print()
+
+    def write(self, file):
+        file.write(f"{self.beg}\t{self.end}\t{self.feature}\n")
+        for annotation in self.annotations:
+            annotation.write(file)
 
 class Annotation(object):
     def __init__(self, tag, description):
@@ -138,7 +199,10 @@ class Annotation(object):
         self.description = description
 
     def print(self):
-        print(f"\t\t\t{self.tag}\t{self.description}\n")
+        print(f"\t\t\t{self.tag}\t{self.description}")
+
+    def write(self, file):
+        file.write(f"\t\t\t{self.tag}\t{self.description}\n")
 
 if __name__ == "__main__":
     main() # type: ignore
