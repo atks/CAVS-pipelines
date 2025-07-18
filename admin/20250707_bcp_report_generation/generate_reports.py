1d5bef89dcf9a6633252ef91b7e18b0fbbb4702b
Adrian Tan <atks@chweekueh.local>
2025-07-18 16:47:55 +0800

    misc changes

--- admin/20250707_bcp_report_generation/generate_reports.py
+++ admin/20250707_bcp_report_generation/generate_reports.py
@@ -24,101 +24,41 @@ import docx
 
 @click.command()
 @click.option(
-    "-m",
-    "--make_file",
-    show_default=True,
-    default="run_ddradseq_population_structure_analysis.mk",
-    help="make file name",
-)
-@click.option(
     "-w",
     "--working_dir",
     default=os.getcwd(),
     show_default=True,
     help="working directory",
 )
+@click.option(
+    "-d",
+    "--data_dir",
+    required = True,
+    show_default=True,
+    help="data directory",
+)
 @click.option("-s", "--sample_file", required=True, help="sample file")
-@click.option("-p", "--population_map_file", required=True, help="population map file")
-@click.option("-g", "--genome_fasta_file", default="", required=False, help="genome FASTA file")
-def main(make_file, working_dir, sample_file, population_map_file, genome_fasta_file):
+def main(working_dir, data_dir):
     """
-    Combine data from 
+    Combine data from 3 BCP files and generate various reports.
 
-    e.g. generate_ddradseq_population_structure_analysis.py
+    e.g. generate_bcp_reports.py
     """
-    print("\t{0:<20} :   {1:<10}".format("make file", make_file))
+    reports_dir = f"{working_dir}/reports"
     print("\t{0:<20} :   {1:<10}".format("working dir", working_dir))
-    print("\t{0:<20} :   {1:<10}".format("sample file", sample_file))
-    print("\t{0:<20} :   {1:<10}".format("population map file", population_map_file))
-    print("\t{0:<20} :   {1:<10}".format("genome fasta file", genome_fasta_file))
+    print("\t{0:<20} :   {1:<10}".format("data dir", data_dir))
+    print("\t{0:<20} :   {1:<10}".format("reports dir", reports_dir))
 
-    # read sample file
-    samples = {}
-    with open(sample_file, "r") as file:
-        for line in file:
-            if not line.startswith("#"):
-                id, fastq1, fastq2 = line.rstrip().split("\t")
-                if id not in samples:
-                    samples[id] = Sample(id, fastq1, fastq2)
-                else:
-                    samples[id].add_fastq(fastq1, fastq2)
-
-    # initialize
-    pg = PipelineGenerator(make_file)
-
-    # create directories in destination folder directory
-    ref_dir = f"{working_dir}/ref"
-    log_dir = f"{working_dir}/log"
-    stats_dir = f"{working_dir}/stats"
-    plot_dir = f"{working_dir}/plot"
-    fastq_dir = f"{working_dir}/fastq"
-    fastq_ulen_dir = f"{working_dir}/fastq_ulen"
-    bam_dir = f"{working_dir}/bam"
-    vcf_dir = f"{working_dir}/vcf"
-    annotation_dir = f"{working_dir}/annotations"
-    denovo_stacks_dir = f"{working_dir}/denovo_stacks"
-    ref_stacks_dir = f"{working_dir}/ref_stacks"
-    qc_dir = f"{working_dir}/qc"
+    # create directories
     try:
-        os.makedirs(ref_dir, exist_ok=True)
-        os.makedirs(log_dir, exist_ok=True)
-        os.makedirs(stats_dir, exist_ok=True)
-        os.makedirs(f"{stats_dir}/coverage", exist_ok=True)
-        os.makedirs(f"{stats_dir}/general", exist_ok=True)
-        os.makedirs(f"{stats_dir}/flag", exist_ok=True)
-        os.makedirs(f"{stats_dir}/idx", exist_ok=True)
-        os.makedirs(plot_dir, exist_ok=True)
-        os.makedirs(fastq_dir, exist_ok=True)
-        os.makedirs(fastq_ulen_dir, exist_ok=True)
-        os.makedirs(bam_dir, exist_ok=True)
-        os.makedirs(vcf_dir, exist_ok=True)
-        os.makedirs(denovo_stacks_dir, exist_ok=True)
-        os.makedirs(ref_stacks_dir, exist_ok=True)
-        os.makedirs(qc_dir, exist_ok=True)
+        os.makedirs(reports_dir, exist_ok=True)
     except OSError as error:
         print(f"{error.filename} cannot be created")
 
+    # read bcp files
     
     
 
-class Sample(object):
-
-    def __init__(self, id, fastq1, fastq2):
-        self.id = id
-        self.fastq1s = []
-        self.fastq1s.append(fastq1)
-        self.fastq2s = []
-        self.fastq2s.append(fastq2)
-
-    def add_fastq(self, fastq1, fastq2):
-        self.fastq1s.append(fastq1)
-        self.fastq2s.append(fastq2)
-
-    def print(self):
-        print(f"id       : {self.id}")
-        print(f"no files : {len(self.fastq1s)}")
-        print(f"fastq1s  : {','.join(self.fastq1s)}")
-        print(f"fastq2s  : {','.join(self.fastq2s)}")
 
 
 if __name__ == "__main__":
