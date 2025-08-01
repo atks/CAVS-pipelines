33720920a57b4f907f97841f4dd69db74d36a7a7
atks <atks@umich.edu>
2025-08-01 14:58:55 +0800

    misc changes

--- vfp/20241210_pangolin_ddradseq/vcf_to_plink.py
+++ vfp/20241210_pangolin_ddradseq/vcf_to_plink.py
@@ -20,20 +20,19 @@
 
 import os
 import click
-from random import sample
 
 @click.command()
 @click.argument("vcf_file")
-@click.option("-s", "--sample_file", default=100, help="sample file")
-@click.option("-o", "--output_plink_file_base_name", default=100, help="plink file base name - two files .ped and .map")
+@click.option("-s", "--sample_file", required=True, help="sample file")
+@click.option("-o", "--output_plink_file_base_name", default="plink", help="plink file base name - two files .ped and .map")
 def main(vcf_file, sample_file, output_plink_file_base_name):
     """
     Convert VCF file to plink format.
 
     e.g. vcf_to_plink.py
     """
-    print("\t{0:<20} :   {1:<10}".format("vcf file", vcf_file))
-    print("\t{0:<20} :   {1:<10}".format("sample file", sample_file))
+    print("\t{0:<20} :   {1:<10}".format("vcf file                   ", vcf_file))
+    print("\t{0:<20} :   {1:<10}".format("sample file                ", sample_file))
     print("\t{0:<20} :   {1:<10}".format("output plink file base name", output_plink_file_base_name))
 
     # Family ID
@@ -60,6 +59,16 @@ def main(vcf_file, sample_file, output_plink_file_base_name):
     no_variants = 0
     no_samples = 0
     
+    SAMPLES = {}
+
+    with open(sample_file, "r") as sa:
+        for line in sa:
+            if not line.startswith("#"):
+                sample_id, latitude, longitude, sex = line.rstrip().split("\t")
+                SAMPLES[sample_id] = Sample(sample_id, latitude, longitude, sex)
+
+    print("{0:<20} :   {1:<10}".format("no. of samples in sample file", len(SAMPLES)))
+
     samples = []
     families = []
     with open(lgen_file, "w") as lgen:
@@ -70,6 +79,7 @@ def main(vcf_file, sample_file, output_plink_file_base_name):
                         if line.startswith("#CHROM"):
                             samples = line.rstrip().split("\t")[9:]
                             no_samples = len(samples)
+                            print("{0:<20} :   {1:<10}".format("no. of samples in vcf file", no_samples))
                             with open(fam_file, "w") as fam:
                                 for idx, sample in enumerate(samples):
                                     # Family ID
@@ -81,14 +91,23 @@ def main(vcf_file, sample_file, output_plink_file_base_name):
                                     family_id = f"FAM{idx+1:2d}"
                                     sample_id = sample
                                     families.append(family_id)
-                                    samples.append(sample_id)
-                                    fam.write(f"{family_id} {sample_id} -1 -1 0 0\n")
+                                    sex_code = -1
+                                    if sample_id in SAMPLES:
+                                        if SAMPLES[sample_id].sex == "M":
+                                            sex_code = 1
+                                        elif SAMPLES[sample_id].sex == "F":
+                                            sex_code = 2
+                                    fam.write(f"{family_id}\t{sample_id}\t-1\t-1\t{sex_code}\t0\n")
                     else:
+                        print(f"Processing variant record line: {line.rstrip()}")
+                        no_variants += 1
+                        if no_variants % 1000 == 0:
+                            print(f"\r{no_variants} variants processed", end="")
+                    
                         chrom, pos, id, ref, alt, qual, filter, info, format, *genotypes = line.rstrip().split("\t")
-                        ns, dp, ad, af = info.split(";")
                         # chromosome (1-22, X, Y or 0 if unplaced)
                         # rs# or snp identifier
-                        # Genetic distance (morgans)
+                        # Genetic distance (morgans) 
                         # Base-pair position (bp units)
                         map.write(f"{chrom} {id} 0 {pos}\n")
                         
@@ -118,6 +137,13 @@ def main(vcf_file, sample_file, output_plink_file_base_name):
                                         allele1 = alt
                                         allele2 = alt
                                     lgen.write(f"{families[idx]}\t{samples[idx]}\t{id}\t{allele1}\t{allele2}\n")
+
+class Sample:
+    def __init__(self, sample_id, latitude, longitude, sex):
+        self.sample_id = sample_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.sex = sex
 
 if __name__ == "__main__":
     main() # type: ignore
