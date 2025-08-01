ef3dcd36418f645ed7995697d929a49593391a9b
atks <atks@umich.edu>
2025-08-01 11:25:46 +0800

    first version

---
+++ vfp/20241210_pangolin_ddradseq/vcf_to_plink.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+
+# The MIT License
+# Copyright (c) 2025 Adrian Tan <adrian_tan@nparks.gov.sg>
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+import os
+import click
+from random import sample
+
+@click.command()
+@click.argument("vcf_file")
+@click.option("-s", "--sample_file", default=100, help="sample file")
+@click.option("-o", "--output_plink_file_base_name", default=100, help="plink file base name - two files .ped and .map")
+def main(vcf_file, sample_file, output_plink_file_base_name):
+    """
+    Convert VCF file to plink format.
+
+    e.g. vcf_to_plink.py
+    """
+    print("\t{0:<20} :   {1:<10}".format("vcf file", vcf_file))
+    print("\t{0:<20} :   {1:<10}".format("sample file", sample_file))
+    print("\t{0:<20} :   {1:<10}".format("output plink file base name", output_plink_file_base_name))
+
+    # Family ID
+    # Individual ID
+    # Paternal ID
+    # Maternal ID
+    # Sex (1=male; 2=female; other=unknown)
+    # Phenotype
+    fam_file = f"{output_plink_file_base_name}.fam"
+    
+    # chromosome (1-22, X, Y or 0 if unplaced)
+    # rs# or snp identifier
+    # Genetic distance (morgans)
+    # Base-pair position (bp units)
+    map_file = f"{output_plink_file_base_name}.map"
+
+    # family ID
+    # individual ID
+    # snp ID
+    # allele 1 of this genotype
+    # allele 2 of this genotype             
+    lgen_file = f"{output_plink_file_base_name}.lgen"
+   
+    no_variants = 0
+    no_samples = 0
+    
+    samples = []
+    families = []
+    with open(lgen_file, "w") as lgen:
+        with open(map_file, "w") as map:    
+            with open(vcf_file, "r") as vcf:
+                for line in vcf:
+                    if line.startswith("#"):
+                        if line.startswith("#CHROM"):
+                            samples = line.rstrip().split("\t")[9:]
+                            no_samples = len(samples)
+                            with open(fam_file, "w") as fam:
+                                for idx, sample in enumerate(samples):
+                                    # Family ID
+                                    # Individual ID
+                                    # Paternal ID
+                                    # Maternal ID
+                                    # Sex (1=male; 2=female; other=unknown)
+                                    # Phenotype
+                                    family_id = f"FAM{idx+1:2d}"
+                                    sample_id = sample
+                                    families.append(family_id)
+                                    samples.append(sample_id)
+                                    fam.write(f"{family_id} {sample_id} -1 -1 0 0\n")
+                    else:
+                        chrom, pos, id, ref, alt, qual, filter, info, format, *genotypes = line.rstrip().split("\t")
+                        ns, dp, ad, af = info.split(";")
+                        # chromosome (1-22, X, Y or 0 if unplaced)
+                        # rs# or snp identifier
+                        # Genetic distance (morgans)
+                        # Base-pair position (bp units)
+                        map.write(f"{chrom} {id} 0 {pos}\n")
+                        
+                        # family ID
+                        # individual ID
+                        # snp ID
+                        # allele 1 of this genotype
+                        # allele 2 of this genotype 
+                        for idx, genotype in enumerate(genotypes):
+                            sample_id = samples[idx]
+                            if genotype == "./.":
+                                lgen.write(f"{families[idx]}\t{samples[idx]}\t{id}\t0 0\n")
+                            else:
+                                gt, dp, ad, gq, gl = genotype.split(":")
+                                if gt == "./.":
+                                   lgen.write(f"{families[idx]}\t{samples[idx]}\t{id}\t0 0\n")
+                                else:
+                                    allele1 = 0
+                                    allele2 = 0
+                                    if gt == "0/0":
+                                        allele1 = ref
+                                        allele2 = ref
+                                    elif gt == "0/1":
+                                        allele1 = ref
+                                        allele2 = alt
+                                    elif gt == "1/1":
+                                        allele1 = alt
+                                        allele2 = alt
+                                    lgen.write(f"{families[idx]}\t{samples[idx]}\t{id}\t{allele1}\t{allele2}\n")
+
+if __name__ == "__main__":
+    main() # type: ignore
